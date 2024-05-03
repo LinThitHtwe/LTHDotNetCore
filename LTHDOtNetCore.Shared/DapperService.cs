d809ad0aefe28c54420bc6c9fcdc7dd094c62284
LinThitHtwe <linthit2745@gmail.com>
2024-05-03 16:28:05 +0630

    [add] shared dapper service {Day 7}

---
+++ LTHDOtNetCore.Shared/DapperService.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LTHDOtNetCore.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> Query<T>(string query,object? param = null)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            var list = dbConnection.Query<T>(query, param).ToList();
+            return list;
+        }
+
+        public int Execute(string query, object? param = null)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            var result = dbConnection.Execute(query, param);
+            return result;
+        }
+    }
+}
