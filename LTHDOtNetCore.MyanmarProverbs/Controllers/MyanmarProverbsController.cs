86e147aa85f502abf3cf412ccf3524d9fa5e679a
LinThitHtwe <linthit2745@gmail.com>
2024-05-13 11:20:47 +0630

    [add] Myanmar Proverbs

---
+++ LTHDOtNetCore.MyanmarProverbs/Controllers/MyanmarProverbsController.cs
@@ -0,0 +1,66 @@
+﻿using LTHDOtNetCore.MyanmarProverbs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace LTHDOtNetCore.MyanmarProverbs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarProverbsController : ControllerBase
+    {
+
+        [HttpGet("ProverbsTitles")]
+        public async Task<IActionResult> GetAllMMProverbsTitlesAsync()
+        {
+            var mmProverbs = JsonConvert.DeserializeObject<MyanmarProverbsModel>(await GetJsonDataAsync());
+            return Ok(mmProverbs!.MmProverbsTitles);
+        }
+
+        [HttpGet("ProverbNames")]
+        public async Task<IActionResult> GetAllProverbNamesAsync()
+        {
+            var mmProverbs = JsonConvert.DeserializeObject<MyanmarProverbsModel>(await GetJsonDataAsync());
+            var proverbNames = mmProverbs!.MmProverbs.Select(mmProverb => new MmProverbNamesResponseDTO()
+            {
+                ProverbId = mmProverb.ProverbId,
+                ProverbName = mmProverb.ProverbName,
+                TitleId = mmProverb.TitleId,
+            }).ToList();
+
+            return Ok(proverbNames);
+        }
+
+        [HttpGet("ProverbNames/Title/{titleId}")]
+        public async Task<IActionResult> GetProverbNamesByTitleId(int titleId)
+        {
+            var mmProverbs = JsonConvert.DeserializeObject<MyanmarProverbsModel>(await GetJsonDataAsync());
+            var proverbNames = mmProverbs!.MmProverbs
+                              .Where(mmProverb => mmProverb.TitleId == titleId)
+                              .Select(mmProverb => new MmProverbNamesResponseDTO()
+                              {
+                                  ProverbId = mmProverb.ProverbId,
+                                  ProverbName = mmProverb.ProverbName,
+                                  TitleId = mmProverb.TitleId,
+                              }).ToList();
+            return Ok(proverbNames);
+        }
+
+        [HttpGet("ProverbDetail/{proverbId}")]
+        public async Task<IActionResult> GetProverbDetailByPorverbId(int proverbId)
+        {
+            var mmProverbs = JsonConvert.DeserializeObject<MyanmarProverbsModel>(await GetJsonDataAsync());
+            var proverbDetail = mmProverbs!.MmProverbs
+                              .FirstOrDefault(mmProverb => mmProverb.ProverbId == proverbId);
+                              
+            return Ok(proverbDetail);
+        }
+
+        private static async Task<string> GetJsonDataAsync()
+        {
+            string jsonString = await System.IO.File.ReadAllTextAsync("MyanmarProverbs.json");
+            return jsonString;
+        }
+    }
+}
---
+++ LTHDOtNetCore.MyanmarProverbs/Models/MyanmarProverbsModel.cs
@@ -0,0 +1,31 @@
+﻿namespace LTHDOtNetCore.MyanmarProverbs.Models
+{
+
+    public class MyanmarProverbsModel
+    {
+        public MmProverbsTitle[] MmProverbsTitles { get; set; }
+        public MmProverb[] MmProverbs { get; set; }
+    }
+
+    public class MmProverbsTitle
+    {
+        public int TitleId { get; set; }
+        public string TitleName { get; set; }
+    }
+
+    public class MmProverb
+    {
+        public int TitleId { get; set; }
+        public int ProverbId { get; set; }
+        public string ProverbName { get; set; }
+        public string ProverbDesp { get; set; }
+    }
+
+    public class MmProverbNamesResponseDTO
+    {
+        public int TitleId { get; set; }
+        public int ProverbId { get; set; }
+        public string ProverbName { get; set; }
+    }
+
+}
