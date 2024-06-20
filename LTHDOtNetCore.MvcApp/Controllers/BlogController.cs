6b71bd4705c78148daddc5d465d752fca0f604a8
LinThitHtwe <linthit2745@gmail.com>
2024-06-20 18:53:00 +0630

    [add] mvc blogs list

---
+++ LTHDOtNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LTHDOtNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var blogs = await _context.Blogs.ToListAsync();
+            return View(blogs);
+        }
+    }
+}
---
+++ LTHDOtNetCore.MvcApp/Models/BlogModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTHDOtNetCore.MvcApp
+{
+    [Table("blog")]
+    public class BlogModel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("author")]
+        public string Author { get; set; }
+        [Column("blogContent")]
+        public string Content { get; set; }
+    }
+}
