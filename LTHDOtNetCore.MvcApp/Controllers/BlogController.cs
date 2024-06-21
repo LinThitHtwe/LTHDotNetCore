7eb18e797614205ca09ab5e25603c5b43d34d24d
LinThitHtwe <linthit2745@gmail.com>
2024-06-21 11:04:50 +0630

    [add] blog update mvc

--- LTHDOtNetCore.MvcApp/Controllers/BlogController.cs
+++ LTHDOtNetCore.MvcApp/Controllers/BlogController.cs
@@ -14,7 +14,7 @@ namespace LTHDOtNetCore.MvcApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var blogs = await _context.Blogs.ToListAsync();
+            var blogs = await _context.Blogs.OrderByDescending(blog => blog.Id).ToListAsync();
             return View(blogs);
         }
 
@@ -43,5 +43,38 @@ namespace LTHDOtNetCore.MvcApp.Controllers
             return Redirect("/Blog");
         }
 
+        [ActionName("Edit")]
+        public async Task<IActionResult> BlogEdit(int id)
+        {
+            var blog = await _context.Blogs.FirstOrDefaultAsync(b=> b.Id == id);
+            if(blog is null)
+            {
+                return Redirect("/Error");
+            }
+            return View("BlogEdit",blog);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> BlogEdit(int id, BlogModel requestBlog)
+        {
+            var blog = await _context.Blogs.FirstOrDefaultAsync(b => b.Id == id);
+            if (blog is null)
+            {
+                return Redirect("/Error");
+            }
+
+            blog.Title = requestBlog.Title;
+            blog.Author = requestBlog.Author;
+            blog.Content = requestBlog.Content;
+
+            var result = await _context.SaveChangesAsync();
+            if (result < 1)
+            {
+                return Redirect("/Error");
+            }
+
+            return Redirect("/Blog");
+        }
     }
 }
