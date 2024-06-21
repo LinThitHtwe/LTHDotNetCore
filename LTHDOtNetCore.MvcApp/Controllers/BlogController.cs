3f21f3787fd2042d6c49a7f50d005cf56a2860de
LinThitHtwe <linthit2745@gmail.com>
2024-06-21 09:40:25 +0630

    [add] blog create mvc

--- LTHDOtNetCore.MvcApp/Controllers/BlogController.cs
+++ LTHDOtNetCore.MvcApp/Controllers/BlogController.cs
@@ -17,5 +17,31 @@ namespace LTHDOtNetCore.MvcApp.Controllers
             var blogs = await _context.Blogs.ToListAsync();
             return View(blogs);
         }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public async Task<IActionResult> BlogCreate(BlogModel requestBlog)
+        {
+            if (requestBlog == null)
+            {
+                return Redirect("/Error");
+            }
+            await _context.Blogs.AddAsync(requestBlog);
+            var result = await _context.SaveChangesAsync();
+            
+            if(result < 1)
+            {
+                return Redirect("/Error");
+            }
+
+            return Redirect("/Blog");
+        }
+
     }
 }
