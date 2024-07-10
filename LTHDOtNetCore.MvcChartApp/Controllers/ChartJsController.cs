105e19fae0098904bccbebaacb337465c76f4874
LinThitHtwe <linthit2745@gmail.com>
2024-07-10 22:43:34 +0630

    [add] chartJs example

---
+++ LTHDOtNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LTHDOtNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+    }
+}
