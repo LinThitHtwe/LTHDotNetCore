b6b22cbf8955878ceb12391e06e75330a0652b80
LinThitHtwe <linthit2745@gmail.com>
2024-07-12 14:32:53 +0630

    [Add] Canvas Chart Example

---
+++ LTHDOtNetCore.MvcChartApp/Controllers/CanvasChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LTHDOtNetCore.MvcChartApp.Controllers
+{
+    public class CanvasChartController : Controller
+    {
+        public IActionResult SimpleLineChart()
+        {
+            return View();
+        }
+    }
+}
