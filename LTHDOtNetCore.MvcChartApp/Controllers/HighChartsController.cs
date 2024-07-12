3a63454a4f10d57c2ed66de090ced5710a7ce3a6
LinThitHtwe <linthit2745@gmail.com>
2024-07-12 14:23:00 +0630

    [Add] HighCharts Example

---
+++ LTHDOtNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LTHDOtNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
