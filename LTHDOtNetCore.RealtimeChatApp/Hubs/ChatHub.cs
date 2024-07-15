0a8262b27f5a2dd58a7132397e368b87cb51255c
LinThitHtwe <linthit2745@gmail.com>
2024-07-15 14:26:16 +0630

    [add] signalR example

---
+++ LTHDOtNetCore.RealtimeChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LTHDOtNetCore.RealtimeChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SeverReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientRecieveMessage", user, message);
+        }
+    }
+}
